c2ff665c8ccd841d6d9b882ea0dd6d521b4376b5
Kornei Dontsov <KorneiDontsov@gmail.com>
2020-12-06 04:21:54 +0300

    Minor refactoring

--- src/libs/KorneiDontsov.Sql.Postgres/core/NpgsqlExceptions.cs
+++ src/libs/KorneiDontsov.Sql.Postgres/core/NpgsqlExceptions.cs
@@ -3,15 +3,15 @@
 	using System;
 
 	static class NpgsqlExceptions {
-		public static SqlException? Match (Exception ex) =>
+		public static SqlException? MatchToSqlException (Exception ex) =>
 			ex switch {
 				PostgresException pgEx =>
 					pgEx.SqlState switch {
 						PostgresErrorCodes.SerializationFailure =>
-							new SqlException.ConflictFailure(SqlConflict.SerializationFailure, pgEx),
+							new SqlException.ConflictFailure(SqlConflict.SerializationFailure, ex),
 						PostgresErrorCodes.UniqueViolation =>
-							new SqlException.ConflictFailure(SqlConflict.UniqueViolation, pgEx),
-						_ => new SqlException(innerException: pgEx)
+							new SqlException.ConflictFailure(SqlConflict.UniqueViolation, ex),
+						_ => new SqlException(innerException: ex)
 					},
 				NpgsqlException _ => new SqlException(innerException: ex),
 				_ => null
